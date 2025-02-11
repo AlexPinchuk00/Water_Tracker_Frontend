c3bffc9592cdafb4079d41ee9abce4dcd1d199e5
LazyStudent23 <wizard.alex2016@gmail.com>
2025-02-12 00:44:15 +0200

    Update DailyNormaModal.jsx

--- src/components/Home/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/Home/DailyNormaModal/DailyNormaModal.jsx
@@ -1,161 +1,187 @@
-import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateWaterRateThunk } from '../../../redux/auth/authOperations';
+import { selectUser } from '../../../redux/auth/authSelectors';
+import { toast } from 'react-toastify';
+import { BaseModalWindow, ContentLoader } from 'components';
+import { selectIsLoading } from '../../../redux/root/rootSelectors';
 import {
+  BoxModal,
   ButtonSave,
-  CalculateWater,
-  Container,
-  Description,
+  Comment,
   Form,
   FormRadio,
-  FormSubTitle,
+  FormResult,
   Formula,
   Input,
   InputRadio,
   Paragraph,
-  Wrapper,
-  Result,
+  TitleModal,
 } from './DailyNormaModal.styled';
-import { updateWaterRate } from '../../../redux/Api/api';
-import { BaseModalWindow } from '../../common/BaseModalWindow/BaseModalWindow';
 
 export const DailyNormaModal = ({ onClose, onShow }) => {
-  const [gender, setGender] = useState('woman');
+  const dispatch = useDispatch();
+  const { gender: reduxGender, waterRate } = useSelector(selectUser);
+  const { isLoading } = useSelector(selectIsLoading);
+
+  const [gender, setGender] = useState(reduxGender);
   const [weight, setWeight] = useState('');
-  const [timeOfActive, setTimeOfActive] = useState('');
-  const [dailyWaterNorm, setDailyWaterNorm] = useState('');
+  const [activityTime, setActivityTime] = useState('');
+  const [dailyIntake, setDailyIntake] = useState((waterRate / 1000).toFixed(1));
   const [intakeGoal, setIntakeGoal] = useState('');
 
-  const dispatch = useDispatch();
-
-  const calculateWaterDailyNorm = useCallback(() => {
-    if (!weight || !timeOfActive) return setDailyWaterNorm('');
-
-    if (weight <= 0 || timeOfActive <= 0) {
-      return toast.error('Please enter a valid data');
-    }
-
-    const userGender = gender === 'woman' ? 0.03 : 0.04;
-    const activityTime = gender ? 0.4 : 0.6;
-
-    const intake = weight * userGender + (timeOfActive / 60) * activityTime;
-
-    setDailyWaterNorm(intake.toFixed(2));
-  }, [gender, weight, timeOfActive]);
+  const calculateWaterIntake = useCallback(() => {
+    if (!weight || !activityTime) return;
+    const factor = gender === 'female' ? 0.03 : 0.04;
+    const activityFactor = gender === 'female' ? 0.4 : 0.6;
+    const intake = (
+      weight * factor +
+      (activityTime / 60) * activityFactor
+    ).toFixed(2);
+    setDailyIntake(intake);
+  }, [gender, weight, activityTime]);
 
   useEffect(() => {
-    calculateWaterDailyNorm();
-  }, [calculateWaterDailyNorm]);
+    calculateWaterIntake();
+  }, [calculateWaterIntake]);
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const handleIntakeGoalChange = e => {
+    const value = e.target.value;
+    const newIntakeGoal = parseFloat(value);
 
-    const userGoal = parseFloat(intakeGoal);
+    if (!isNaN(newIntakeGoal) && newIntakeGoal >= 1) {
+      setIntakeGoal(newIntakeGoal);
+    } else if (value === '') {
+      setIntakeGoal('');
+    }
+  };
 
-    const finishGoal = userGoal ? userGoal : dailyWaterNorm;
+  const handleSave = () => {
+    const parsedDailyIntake = parseFloat(dailyIntake);
 
-    dispatch(updateWaterRate({ waterRate: finishGoal }));
-    onClose();
+    const isDataValid = (weight > 0 && activityTime > 0) || intakeGoal > 0;
+
+    if (!isDataValid) {
+      toast.error(
+        'Please fill in all fields or enter your intake goal before saving.',
+      );
+      return;
+    }
+
+    if (isNaN(parsedDailyIntake) || parsedDailyIntake <= 0) {
+      toast.error('Please enter a valid intake goal.');
+      return;
+    }
+
+    dispatch(
+      updateWaterRateThunk(intakeGoal ? intakeGoal : parsedDailyIntake),
+    ).then(data => {
+      if (!data.error) {
+        onClose();
+        setIntakeGoal('');
+        setWeight('');
+        setActivityTime('');
+      }
+    });
   };
 
   return (
     <BaseModalWindow onClose={onClose} onShow={onShow} title="My daily norma">
-      <Container>
+      <BoxModal>
         {
-          <Wrapper>
+          <div>
             <Formula>
               <Paragraph>
-                For woman:<span> V=(M*0,03) + (T*0,4)</span>
+                For girl: <span>V=(M*0,03) + (T*0,4)</span>
               </Paragraph>
               <Paragraph>
-                For man:<span> V=(M*0,04) + (T*0,6)</span>
+                For man: <span>V=(M*0,04) + (T*0,6)</span>
               </Paragraph>
             </Formula>
-            <Description>
+            <Comment>
               <p>
-                <span>*</span>V is the volume of the water norm in liters per
+                <span>*</span> V is the volume of the water norm in liters per
                 day, M is your body weight, T is the time of active sports, or
                 another type of activity commensurate in terms of loads (in the
                 absence of these, you must set 0)
               </p>
-            </Description>
-          </Wrapper>
+            </Comment>
+          </div>
         }
-
         {
-          <Form>
-            <FormRadio>
-              <FormSubTitle>Calculate your rate:</FormSubTitle>
-              <label>
-                <InputRadio
-                  type="radio"
-                  name="gender"
-                  value="woman"
-                  checked={gender === 'woman'}
-                  onChange={() => setGender('woman')}
+          <div>
+            <Form>
+              <FormRadio>
+                <TitleModal>Calculate your rate:</TitleModal>
+                <label>
+                  <InputRadio
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    checked={gender === 'female'}
+                    onChange={() => setGender('female')}
+                  />
+                  <span>For girl</span>
+                </label>
+                <label>
+                  <InputRadio
+                    type="radio"
+                    name="gender"
+                    value="male"
+                    checked={gender === 'male'}
+                    onChange={() => setGender('male')}
+                  />
+                  <span>For man</span>
+                </label>
+              </FormRadio>
+              <div>
+                <Paragraph>Your weight in kilograms:</Paragraph>
+                <Input
+                  type="number"
+                  min="0"
+                  max="250"
+                  placeholder="0"
+                  value={weight}
+                  onChange={e => setWeight(e.target.value)}
                 />
-                <span>For woman</span>
-              </label>
-              <label>
-                <InputRadio
-                  type="radio"
-                  name="gender"
-                  value="man"
-                  checked={gender === 'man'}
-                  onChange={() => setGender('man')}
+              </div>
+              <div>
+                <Paragraph>
+                  Time of active participation in sports or other activities
+                  with a high physical load:
+                </Paragraph>
+                <Input
+                  type="number"
+                  min="0"
+                  placeholder="0"
+                  value={activityTime}
+                  onChange={e => setActivityTime(e.target.value)}
                 />
-                <span>For man</span>
-              </label>
-            </FormRadio>
-            <div>
-              <Paragraph>Your weight in kilograms:</Paragraph>
-              <Input
-                type="number"
-                name="weight"
-                value={weight}
-                onChange={e =>
-                  setWeight(e.target.value.replace(/[^0-9.]/g, ''))
-                }
-              />
-            </div>
-            <div>
-              <Paragraph>
-                The time of active participation in sports or other activities
-                with a high physical. load in hours:
-              </Paragraph>
-              <Input
-                type="number"
-                name="timeOfActive"
-                value={timeOfActive}
-                onChange={e =>
-                  setTimeOfActive(e.target.value.replace(/[^0-9.]/g, ''))
-                }
-              />
-            </div>
-            <CalculateWater>
-              <Result>The required amount of water in liters per day:</Result>
-              <span>
-                {dailyWaterNorm ? parseFloat(dailyWaterNorm).toFixed(1) : 0} L
-              </span>
-            </CalculateWater>
-            <div>
-              <FormSubTitle>
-                Write down how much water you will drink:
-              </FormSubTitle>
-
-              <Input
-                type="number"
-                name="intakeGoal"
-                value={intakeGoal}
-                onChange={e => setIntakeGoal(e.target.value)}
-              />
-            </div>
-            <ButtonSave type="submit" onClick={handleSubmit}>
-              Save
-            </ButtonSave>
-          </Form>
+              </div>
+              <FormResult>
+                The required amount of water in liters per day:
+                <strong> {parseFloat(dailyIntake).toFixed(1)}L</strong>
+              </FormResult>
+              <div>
+                <TitleModal>
+                  Write down how much water you will drink:
+                </TitleModal>
+                <Input
+                  type="number"
+                  placeholder="0"
+                  value={intakeGoal}
+                  onChange={handleIntakeGoalChange}
+                />
+              </div>
+              <ButtonSave onClick={handleSave}>
+                Save {isLoading && <ContentLoader />}
+              </ButtonSave>
+            </Form>
+          </div>
         }
-      </Container>
+      </BoxModal>
     </BaseModalWindow>
   );
 };
+
