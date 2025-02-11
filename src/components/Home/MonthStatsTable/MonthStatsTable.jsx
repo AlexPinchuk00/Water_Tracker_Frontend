7297cdf056a75fe2adee0a36719b6eb54808742d
LazyStudent23 <wizard.alex2016@gmail.com>
2025-02-12 01:39:26 +0200

    Update MonthStatsTable.jsx

--- src/components/Home/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/Home/MonthStatsTable/MonthStatsTable.jsx
@@ -1,13 +1,73 @@
-import { useState, useEffect, useRef } from 'react';
-import { format } from 'date-fns';
-import {DaysGeneralStats} from '../DaysGeneralStats/DaysGeneralStats';
-
-
-export const MonthStatsTable = ({ monthDataMap }) => {
-  const [selectedDayStats, setSelectedDayStats] = useState(null);
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMonthWater } from '../../../redux/waterData/waterOperations';
+import {
+  selectMonthData,
+  selectWaterVolumePercentage,
+} from '../../../redux/waterData/waterSelectors';
+import {
+  format,
+  subMonths,
+  addMonths,
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  isSameMonth,
+} from 'date-fns';
+import { DaysGeneralStats } from 'components';
+import {
+  ButtonPaginator,
+  DaysButton,
+  DaysList,
+  DaysPercentage,
+  HeaderMonth,
+  Paginator,
+  Year,
+} from './MonthStatsTable.styled';
+export const MonthStatsTable = () => {
+  const dispatch = useDispatch();
+  const monthData = useSelector(selectMonthData);
+  const [selectedMonth, setSelectedMonth] = useState(new Date());
+  const [activeButton, setActiveButton] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
   const [dayPosition, setDayPosition] = useState({ top: 0, left: 0, width: 0 });
+  const [selectedDayStats, setSelectedDayStats] = useState(null);
+  const [isHovering, setIsHovering] = useState(false);
   const dayRefs = useRef({});
+  const roundedWaterVolumePercentage = useSelector(selectWaterVolumePercentage);
+  const startDate = format(startOfMonth(selectedMonth), 'yyyy-MM-dd');
+  const endDate = format(endOfMonth(selectedMonth), 'yyyy-MM-dd');
+  useEffect(() => {
+    dispatch(getMonthWater({ startDate, endDate }));
+  }, [dispatch, endDate, roundedWaterVolumePercentage, startDate]);
+  const handlePreviousMonth = () => {
+    const newMonth = subMonths(selectedMonth, 1);
+    setSelectedMonth(newMonth);
+    if (isSameMonth(newMonth, new Date())) {
+      setActiveButton(null);
+    } else {
+      setActiveButton('prev');
+    }
+  };
+  const handleNextMonth = () => {
+    if (selectedMonth < new Date()) {
+      const newMonth = addMonths(selectedMonth, 1);
+      setSelectedMonth(newMonth);
+      if (isSameMonth(newMonth, new Date())) {
+        setActiveButton(null);
+      } else {
+        setActiveButton('next');
+      }
+    }
+  };
+  const daysOfMonth = eachDayOfInterval({
+    start: startOfMonth(selectedMonth),
+    end: endOfMonth(selectedMonth),
+  });
+  const monthDataMap = monthData.reduce((acc, dayData) => {
+    acc[dayData.date] = dayData;
+    return acc;
+  }, {});
   const onDayClick = day => {
     const dayKey = format(day, 'yyyy-MM-dd');
     const dayData = monthDataMap[dayKey];
@@ -18,34 +78,36 @@ export const MonthStatsTable = ({ monthDataMap }) => {
     } else {
       setSelectedDayStats({
         date: dayKey,
-        waterVolumeSum: dayData?.waterVolumeSum ?? 0,
-        drinkCount: dayData?.drinkCount ?? 0,
-        waterVolumePercentage: dayData?.waterVolumePercentage ?? 0,
+        waterVolumeSum: dayData ? dayData.waterVolumeSum : 0,
+        drinkCount: dayData ? dayData.drinkCount : 0,
+        waterVolumePercentage: dayData ? dayData.waterVolumePercentage : 0,
       });
       setModalVisible(true);
-    }
-    // Установка позиции для модального окна
-    const dayElement = dayRefs.current[dayKey];
-    if (dayElement) {
-      const rect = dayElement.getBoundingClientRect();
-      setDayPosition({
-        top: rect.top + window.scrollY,
-        left: rect.left,
-        width: rect.width,
-      });
+      // Получаем элемент по ключу dayKey
+      const dayElement = dayRefs.current[dayKey];
+      if (dayElement) {
+        const rect = dayElement.getBoundingClientRect();
+        setDayPosition({
+          top: rect.top + window.scrollY,
+          left: rect.left,
+          width: rect.width,
+        });
+      }
     }
   };
-  // Закрытие модалки при клике вне элемента
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    setSelectedDayStats(null);
+  };
   useEffect(() => {
     const handleClickOutside = event => {
-      if (
-        modalVisible &&
-        selectedDayStats &&
-        dayRefs.current[selectedDayStats.date] &&
-        !dayRefs.current[selectedDayStats.date].contains(event.target)
-      ) {
-        setModalVisible(false);
-        setSelectedDayStats(null);
+      if (modalVisible) {
+        const isClickOutside = Object.values(dayRefs.current).every(
+          ref => ref && !ref.contains(event.target),
+        );
+        if (isClickOutside) {
+          handleCloseModal();
+        }
       }
     };
     document.addEventListener('mousedown', handleClickOutside);
@@ -55,25 +117,61 @@ export const MonthStatsTable = ({ monthDataMap }) => {
   }, [modalVisible, selectedDayStats]);
   return (
     <div>
-      {Object.keys(monthDataMap).map(dayKey => {
-        const dayData = monthDataMap[dayKey];
-        return (
-          <div
-            key={dayKey}
-            ref={el => (dayRefs.current[dayKey] = el)}
-            onClick={() => onDayClick(new Date(dayKey))}
+      <HeaderMonth>
+        <h2>Month</h2>
+        <Paginator
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+        >
+          <ButtonPaginator
+            onClick={handlePreviousMonth}
+            active={activeButton === 'next'}
           >
-            {dayKey}: {dayData?.waterVolumePercentage ?? 0}%
-          </div>
-        );
-      })}
-      {modalVisible && selectedDayStats && (
-        <DaysGeneralStats
-          stats={selectedDayStats}
-          position={dayPosition}
-          onClose={() => setModalVisible(false)}
-        />
-      )}
+            &lt;
+          </ButtonPaginator>
+          <span>{format(selectedMonth, 'MMMM')}</span>
+          {isHovering && (
+            <Year>{format(selectedMonth, 'yyyy').split('-')[0]}</Year>
+          )}
+          <ButtonPaginator
+            onClick={handleNextMonth}
+            disabled={selectedMonth >= new Date()}
+            active={activeButton === 'prev'}
+          >
+            &gt;
+          </ButtonPaginator>
+        </Paginator>
+      </HeaderMonth>
+      <DaysList>
+        {daysOfMonth.map(day => {
+          const dayKey = format(day, 'yyyy-MM-dd');
+          const dayData = monthDataMap[dayKey];
+          const percentage = dayData ? dayData.waterVolumePercentage : 0;
+          const isHighlighted = dayData && dayData.waterVolumePercentage < 100;
+          return (
+            <div key={dayKey}>
+              <DaysPercentage>
+                <DaysButton
+                  ref={el => (dayRefs.current[dayKey] = el)} // Используем dayKey как ключ
+                  onClick={() => onDayClick(day)}
+                  isHighlighted={isHighlighted}
+                >
+                  {format(day, 'd')}
+                </DaysButton>
+                <span>{Math.round(percentage)}%</span>
+              </DaysPercentage>
+            </div>
+          );
+        })}
+        {modalVisible && selectedDayStats && (
+          <DaysGeneralStats
+            stats={selectedDayStats}
+            position={dayPosition}
+            onClose={handleCloseModal}
+            onShow={modalVisible}
+          />
+        )}
+      </DaysList>
     </div>
   );
 };
