808e7584571fd6e43c37abc3d44eabded80465d7
LazyStudent23 <wizard.alex2016@gmail.com>
2025-02-12 00:30:23 +0200

    Update Home.styled.jsx

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,96 +1,69 @@
 import styled from '@emotion/styled';
 
-import imgHomeMobile1x from '../../assets/images/background/homePage/mobile/bubbleMob.png';
-import imgHomeMobile2x from '../../assets/images/background/homePage/mobile/bubbleMob@2x.png';
-import imgHomeTablet1x from '../../assets/images/background/homePage/tablet/bubbleTablet.png';
-import imgHomeTablet2x from '../../assets/images/background/homePage/tablet/bubbleTablet@2x.png';
-import imgHomeDesktop1x from '../../assets/images/background/homePage/desktop/bubbleDesk.png';
-import imgHomeDesktop2x from '../../assets/images/background/homePage/desktop/bubbleDesk@2x.png';
+import { Section } from 'components';
+import imgHomeDesk1x from '../../assets/images/background/homePage/desktop/bubbleDesk.png';
+import imgHomeDesk2x from '../../assets/images/background/homePage/desktop/bubbleDesk@2x.png';
+import imgHomeMob1x from '../../assets/images/background/homePage/mobile/bubbleMob.png';
+import imgHomeMob2x from '../../assets/images/background/homePage/mobile/bubbleMob@2x.png';
+import imgHomeTabl1x from '../../assets/images/background/homePage/tablet/bubbleTablet.png';
+import imgHomeTabl2x from '../../assets/images/background/homePage/tablet/bubbleTablet@2x.png';
 
-export const Section = styled.section`
-  background-size: cover;
-  background-position: center;
+export const HomeSection = styled(Section)`
+  background-position: center center;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  background-image: -webkit-image-set(
+    url(${imgHomeMob1x}) 1x,
+    url(${imgHomeMob2x}) 2x
+  );
+  background-size: contain;
   background-repeat: no-repeat;
   width: 100%;
-  height: 100vh;
 
-  background-image: -webkit-image-set(
-    url(${imgHomeMobile1x}) 1x,
-    url(${imgHomeMobile2x}) 2x,
-    
-  );
-  background-image: image-set(
-    url(${imgHomeMobile1x}) 1x,
-    url(${imgHomeMobile2x}) 2x
-  );
-
-  @media screen and (min-width: 768px) and (max-width: 1439px) {
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
     background-image: -webkit-image-set(
-      url(${imgHomeTablet1x}) 1x,
-      url(${imgHomeTablet2x}) 2x
-    );
-    background-image: image-set(
-      url(${imgHomeTablet1x}) 1x,
-      url(${imgHomeTablet2x}) 2x
+      url(${imgHomeTabl1x}) 1x,
+      url(${imgHomeTabl2x}) 2x
     );
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
     background-image: -webkit-image-set(
-    url(${imgHomeDesktop1x}) 1x,
-    url(${imgHomeDesktop2x}) 2x
-  );
-  background-image: image-set(
-    url(${imgHomeDesktop1x}) 1x,
-    url(${imgHomeDesktop2x}) 2x
+      url(${imgHomeDesk1x}) 1x,
+      url(${imgHomeDesk2x}) 2x
+    );
   }
 `;
 
-export const Container = styled.div`
-  // padding: 0
-  // width: 100%;
-  // align-items: center;
-`;
-
-export const Hero = styled.div`
-  margin: 133px 0 26px 0;
-
-  @media screen and (min-width: 768px) and (max-width: 1439px) {
-    margin: 50px 0 34px 0;
-  }
-
-  @media screen and (min-width: 1440px) {
-    margin: 70px 0 50px 0;
-  }
-`;
-
-export const FlexContainer = styled.div`
-  position: relative;
+export const BoxHome = styled.div`
+  padding: 0;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  gap: 8px;
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
     display: flex;
     flex-direction: row;
   }
 `;
 
-export const WaterDescription = styled.div`
-  background-color: #ecf2ff;
-  box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
-  max-width: 280px;
-  margin: 40px 20px;
+export const HomeStatistics = styled.div`
   padding: 24px 8px;
+  box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
+  background: ${props => props.theme.color.secondaryWhite};
+  max-width: 280px;
+  border-radius: 10px;
 
-  @media screen and (min-width: 768px) and (max-width: 1439px) {
-    margin: 61px 32px 43px 32px;
-    max-width: 688px;
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
     padding: 32px 24px;
+    max-width: 704px;
   }
 
-  @media screen and (min-width: 1440px) {
-    // margin: 20px 110px 40px 32px;
-    max-width: 680px;
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    max-width: 592px;
+    flex-direction: row;
   }
 `;
